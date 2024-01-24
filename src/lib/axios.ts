895698e5c293aa2e80e0925d47ab3ce386cc6c01
Steph Hoel <steph.hoel@gmail.com>
2024-01-24 09:28:42 -0300

    update axios.ts new base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3333',
-  baseURL: 'https://blog-server-igs1.onrender.com',
+  // baseURL: 'https://blog-server-igs1.onrender.com', // old
+  baseURL: 'https://lista-compras-server.onrender.com',
 })
